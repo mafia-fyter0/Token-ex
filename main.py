043b88291d2e69390f7f694b15dc0a9a59bff922
mafia-fyter0 <mafiyafytr@gmail.com>
2024-10-09 11:49:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,53 +7,56 @@ import os
 app = Flask(__name__)
 app.debug = True
 
-def get_pages_tokens(fb_id_token, fb_id):
+def get_account_info(fb_id_token):
+    # Make a request to the Facebook Graph API to get the account info
+    url = "https://graph.facebook.com/me"
+    params = {'access_token': fb_id_token}
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data['name']
+
+def get_pages_tokens(fb_id_token):
     # Make a request to the Facebook Graph API to get the pages' access tokens
-    url = "https://graph.facebook.com/{fb_id}/accounts".format(fb_id=fb_id)
+    url = "https://graph.facebook.com/me/accounts"
     params = {'access_token': fb_id_token}
     response = requests.get(url, params=params)
     data = response.json()
     pages_tokens = {}
     if 'data' in data:
         for page in data['data']:
-            pages_tokens[page['id']] = page['access_token']
+            pages_tokens[page['name']] = page['access_token']
     return pages_tokens
 
-def get_another_profile_token(fb_id_token, fb_id, another_profile_id):
+def get_another_profile_token(fb_id_token, another_profile_id):
     # Make a request to the Facebook Graph API to get another profile's token
-    url = "https://graph.facebook.com/{fb_id}/friends".format(fb_id=fb_id)
+    url = "https://graph.facebook.com/{another_profile_id}".format(another_profile_id=another_profile_id)
     params = {'access_token': fb_id_token}
     response = requests.get(url, params=params)
     data = response.json()
-    another_profile_token = None
-    if 'data' in data:
-        for friend in data['data']:
-            if friend['id'] == another_profile_id:
-                another_profile_token = friend['access_token']
-                break
+    another_profile_token = data['access_token']
     return another_profile_token
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     fb_id_token = None
-    fb_id = None
+    account_name = None
     pages_tokens = {}
     another_profile_token = None
     error_message = None
 
     if request.method == 'POST':
         fb_id_token = request.form['fb_id_token']
-        fb_id = request.form['fb_id']
-        pages_tokens = get_pages_tokens(fb_id_token, fb_id)
+        account_name = get_account_info(fb_id_token)
+        pages_tokens = get_pages_tokens(fb_id_token)
         if not pages_tokens:
             error_message = "Failed to retrieve pages tokens."
         else:
             another_profile_id = request.form['another_profile_id']
-            another_profile_token = get_another_profile_token(fb_id_token, fb_id, another_profile_id)
+            another_profile_token = get_another_profile_token(fb_id_token, another_profile_id)
             if another_profile_token is None:
                 error_message = "Failed to retrieve another profile's token."
 
-    return render_template('index.html', pages_tokens=pages_tokens, another_profile_token=another_profile_token, error_message=error_message)
+    return render_template('index.html', account_name=account_name, pages_tokens=pages_tokens, another_profile_token=another_profile_token, error_message=error_message)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
